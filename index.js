e3917fa846c50e315c86f7d05b8fd88354999168
Eric Thvedt <ericsthvedt@gmail.com>
2023-02-06 08:36:56 -0800

    superbowlWin fn passes tests

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(arr) {
+    let yearIndex = 0;
+    arr.find((obj, index, arr) => {
+        if (obj.result === 'W') {
+            yearIndex = index;
+            return true;
+        }
+    })
+    if (yearIndex != 0) {
+        return arr[yearIndex].year;
+    }
+    else {
+        return undefined;
+    }
+};
